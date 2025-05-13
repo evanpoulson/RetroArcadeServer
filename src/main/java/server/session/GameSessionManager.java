8e559a4fe1c766543c102b8e77d8f5fe1543f2aa
30208763 <evan.poulson@ucalgary.ca>
2025-05-13 13:58:43 -0600

    Started work on GameSessionManager

--- src/main/java/server/session/GameSessionManager.java
+++ src/main/java/server/session/GameSessionManager.java
@@ -1,4 +1,16 @@
 package server.session;
 
-public class GameSessionManager {
+import server.player.PlayerHandler;
+import server.utility.GameType;
+
+public class GameSessionManager implements Runnable{
+
+    public GameSessionManager(PlayerHandler p1, PlayerHandler p2, GameType gameType) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
