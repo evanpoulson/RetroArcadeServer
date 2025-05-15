64c5ba7be4868804cd2b09eac9ca5b27f434e2bd
30208763 <evan.poulson@ucalgary.ca>
2025-05-15 07:44:53 -0600

    Updated message types to enable game session related thread messages.

--- src/main/java/server/utility/MessageType.java
+++ src/main/java/server/utility/MessageType.java
@@ -9,6 +9,7 @@ package server.utility;
  */
 public enum MessageType {
 
+    // System messages
     /**
      * Indicates that a connection has been established or requested.
      */
@@ -22,6 +23,64 @@ public enum MessageType {
     /**
      * Represents an error state or conveys information about a failure.
      */
-    ERROR
+    ERROR,
+
+    // Game control messages
+    /**
+     * Request to pause the game. Can only be sent by the current player.
+     */
+    PAUSE_REQUEST,
+
+    /**
+     * Request to resume the game. Can only be sent by the player who paused it.
+     */
+    RESUME_REQUEST,
+
+    /**
+     * Notification that the game has been paused.
+     */
+    GAME_PAUSED,
+
+    /**
+     * Notification that the game has been resumed.
+     */
+    GAME_RESUMED,
+
+    // Turn management
+    /**
+     * Notification that it is now a player's turn.
+     */
+    YOUR_TURN,
+
+    /**
+     * Notification that it is another player's turn.
+     */
+    OTHER_PLAYER_TURN,
+
+    /**
+     * Notification that a move was ignored because it wasn't the player's turn.
+     */
+    NOT_YOUR_TURN,
+
+    // Game state updates
+    /**
+     * A player has made a move.
+     */
+    MOVE_MADE,
+
+    /**
+     * The game has ended with a winner.
+     */
+    GAME_WON,
+
+    /**
+     * The game has ended in a draw.
+     */
+    GAME_DRAWN,
+
+    /**
+     * The current state of the game board.
+     */
+    GAME_STATE_UPDATE
 }
 
