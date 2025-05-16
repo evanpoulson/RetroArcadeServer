d817a75229f4cbbfe0239d8182882d4a9ba2fd81
30208300 <colby.campbell@ucalgary.ca>
2025-05-15 19:32:35 -0600

    Created the LogLevel enums for the different types of log messages, only INFO and ERROR for now. Finished ServerLogger methods.

---
+++ src/main/java/server/utility/LogLevel.java
@@ -0,0 +1,16 @@
+package server.utility;
+
+/**
+ * Represents the different log levels for a log message
+ */
+public enum LogLevel {
+    /**
+     * Represents a regular information message
+     */
+    INFO,
+
+    /**
+     * Represents an error message
+     */
+    ERROR,
+}
--- src/main/java/server/utility/ServerLogger.java
+++ src/main/java/server/utility/ServerLogger.java
@@ -1,6 +1,10 @@
 package server.utility;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -13,34 +17,104 @@ public class ServerLogger {
     /**
      * Starts the ServerLogger
      */
-    public static void startServerLogger() {}
+    public static void startServerLogger() {
+        updateFilename();
+        createPrintWriter();
+    }
 
     /**
      * Creates a PrintWriter object
      */
-    private static void createPrintWriter() {}
+    private static void createPrintWriter() {
+        try {
+            // Make sure the log file has the necessary directory
+            Files.createDirectories(Paths.get(DIR));
+            // Create the writer object
+            writer = new PrintWriter(new FileWriter(DIR + "\\" + filename, true));
+            // Ensures the log file is saved properly if the server crashes or closed incorrectly
+            Runtime.getRuntime().addShutdownHook(new Thread(ServerLogger::closeServerLogger));
+        } catch (IOException e) {
+            // Could be more robust?
+            System.err.println("!!! CRITICAL ERROR: ServerLogger failed to start. Logging disabled. !!!");
+        }
+    }
 
     /**
      * Updates the filename if different and creates a new PrintWriter object
      */
-    private static void updateFilename() {}
+    private static void updateFilename() {
+        // Get the new filename
+        String updatedFilename = "logs_" + ZonedDateTime.now(java.time.ZoneId.systemDefault()).format(DateTimeFormatter.ISO_LOCAL_DATE);
+        // If the filenames do not match, update the filename, close the old log file and create a new one
+        if (!filename.equals(updatedFilename)) {
+            filename = updatedFilename;
+            closeServerLogger();
+            createPrintWriter();
+        }
+    }
 
     /**
-     * Prints information to the console as well as logs in a text file
+     * Base command, prints information to the console as well as logs in a text file
+     * @param level the severity or importance of the message
      * @param args arguments to be printed and logged
      */
-    public synchronized static void log(Object... args) {}
+    public synchronized static void log(LogLevel level, Object... args) {
+        // Convert the Object arguments into Strings
+        String[] stringArgs = new String[args.length];
+        for (int i = 0; i < args.length; i++) {
+            stringArgs[i] = args[i].toString();
+        }
+
+        // Combine the arguments ino a String and add a timestamp and log level
+        String timestamp = ZonedDateTime.now(java.time.ZoneId.systemDefault())
+                .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        String levelPadded = String.format("%-5s", level.name());
+        String argsStr = String.join(" ", stringArgs);
+        String formattedStr = level == LogLevel.ERROR ? ">>>>" + argsStr + "<<<<" : argsStr;
+        String string = "[" + timestamp + "] [" + levelPadded + "] " + formattedStr;
+
+        // Print the output to the console
+        if (level == LogLevel.ERROR) {
+            System.err.println(string);
+        } else {
+            System.out.println(string);
+        }
+
+        // Log the information in to a text file
+        updateFilename();
+        if (writer != null) {
+            writer.println(string);
+            // Ensure immediate writing
+            writer.flush();
+        }
+    }
+
+    /**
+     * Prints information to the console as a regular message as well as logs in a text file
+     * @param args arguments to be printed and logged
+     */
+    public synchronized static void logInfo(Object... args) {
+        log(LogLevel.INFO, args);
+    }
 
     /**
      * Prints information to the console as an error as well as logs in a text file
      * @param args arguments to be printed and logged
      */
-    public synchronized static void logError(Object... args) {}
+    public synchronized static void logError(Object... args) {
+        log(LogLevel.ERROR, args);
+    }
 
     /**
      * Closes the PrintWriter
      */
-    public synchronized static void closeServerLogger() {}
+    public synchronized static void closeServerLogger() {
+        // Check that the writer is not null
+        if (writer != null) {
+            writer.close();
+            writer = null;
+        }
+    }
 
     /**
      * Get the DIR, used for testing purposes
